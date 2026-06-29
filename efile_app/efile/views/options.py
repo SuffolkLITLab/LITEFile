7a81c250d30d2909d887e6a96df84f37449c970f
Quinten Steenhuis <qsteenhuis@gmail.com>
2026-06-29 17:31:23 -0400

    Expose active durable draft on options page

--- efile_app/efile/views/options.py
+++ efile_app/efile/views/options.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from efile.api.suffolk_api_views import get_tyler_token
+from efile.services.drafts import draft_snapshot, get_active_draft
 
 from ..utils.case_data_utils import get_case_data
 from ..workflow import WorkflowStepKey, get_workflow_context
 
 
+@ensure_csrf_cookie
 def efile_options(request, jurisdiction):
     """Options view that displays saved case data and provides next steps."""
     # Get case data from session
     if request.user.is_authenticated:
         case_data = get_case_data(request)
+        active_draft = get_active_draft(request)
     else:
         case_data = {}
+        active_draft = None
 
     is_logged_in = request.user.is_authenticated
     if not get_tyler_token(request, jurisdiction):
@@ -22,7 +27,8 @@ def efile_options(request, jurisdiction):
     context = {
         "is_logged_in": is_logged_in,
         "case_data": case_data,
-        "has_case_data": bool(case_data),
+        "filing_draft": draft_snapshot(active_draft),
+        "has_case_data": bool(case_data or active_draft),
     }
     context.update(get_workflow_context(WorkflowStepKey.OPTIONS, jurisdiction))
 
