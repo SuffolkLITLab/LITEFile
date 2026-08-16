26ff1994a2708221ad5b470e5e8de696f224c4ad
Quinten Steenhuis <qsteenhuis@suffolk.edu>
2026-08-16 12:17:39 -0400

    Add migration needed for ci/cd

---
+++ efile_app/efile/migrations/0012_merge_filing_plans_and_jurisdiction_accounts_and_workflow.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("efile", "0011_filing_plans"),
+        ("efile", "0011_merge_jurisdiction_accounts_and_workflow"),
+    ]
+
+    operations = []
